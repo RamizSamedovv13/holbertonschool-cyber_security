5c00bf1c9d7019a3e8820a7a6ddf21e44a734929
RamizSəmədovv13 <ramizsmdov11@gmail.com>
2026-05-06 11:50:21 -0400

    Fif2

--- linux_security/0x03_protocols_servers/0-iptables.sh
+++ linux_security/0x03_protocols_servers/0-iptables.sh
@@ -1,17 +1,2 @@
 #!/bin/bash
-
-# root check
-if [[ $EUID -ne 0 ]]; then
-  echo "Error: run with sudo"
-  exit 1
-fi
-
-# iptables check
-if ! command -v iptables &> /dev/null; then
-  echo "Error: iptables not found on this system"
-  exit 1
-fi
-
-# show rules
-echo "=== IPTABLES RULES ==="
-iptables -L -n -v --line-numbers
+sudo iptables -L -n -v --line-numbers
